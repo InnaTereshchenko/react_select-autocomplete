7fb2e1201376365cc897fccd9e95836aae1f49b2
Inna <inna.tereshchenko1@gmail.com>
2020-01-13 21:29:00 +0200

    Updates

---
+++ precache-manifest.96f383a12d2e2290683111843d8aa570.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f9cb854e7e9387f9c323115f3ce206",
+    "url": "/react_select-autocomplete/index.html"
+  },
+  {
+    "revision": "cf5de5a4589b7911efba",
+    "url": "/react_select-autocomplete/static/css/main.99e48cb5.chunk.css"
+  },
+  {
+    "revision": "29ba6a5161fc628e56be",
+    "url": "/react_select-autocomplete/static/js/2.8cef289f.chunk.js"
+  },
+  {
+    "revision": "cf5de5a4589b7911efba",
+    "url": "/react_select-autocomplete/static/js/main.70a9a2a1.chunk.js"
+  },
+  {
+    "revision": "c507b82e3e4e5e5aa023",
+    "url": "/react_select-autocomplete/static/js/runtime~main.6b0ab126.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.691d3bdca926b05a4c625169298fc4a3.js"
+  "/react_select-autocomplete/precache-manifest.96f383a12d2e2290683111843d8aa570.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_select-autocomplete/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
